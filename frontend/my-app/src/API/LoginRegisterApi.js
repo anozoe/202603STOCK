7d1a00cf2a9bbffe4fff49bc8b9438669b4a1ffb
KeisukeYunoki <qifuyoumu@gmail.com>
2026-04-06 12:00:58 +0900

    ログイン新規登録コンポーネント化前

---
+++ frontend/my-app/src/API/LoginRegisterApi.js
@@ -0,0 +1,15 @@
+// TODO: 担当者が実装予定
+export const loginApi = async (username, password) => {
+  console.warn("Login API is not implemented yet");
+  return { success: true, token: "dummy-token" };
+};
+
+export const registerApi = async (userData) => {
+  console.warn("Register API is not implemented yet");
+  return { success: true };
+};
+
+export const fetchJson = async (url, options) => {
+  console.warn("fetchJson is not implemented yet");
+  return {};
+};
---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage'
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
+export default App;
