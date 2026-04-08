53362733cfb515751f3b6e63aa87d3c428ceb357
KeisukeYunoki <qifuyoumu@gmail.com>
2026-04-08 15:34:51 +0900

    no51-55修正

--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -1,30 +1,38 @@
 import React from 'react'
-import '../styls/Header.css'
-import { useLocation } from 'react-router-dom'
+import '../styles/Header.css'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const pageTitles = {
     '/header': 'ヘッダ', //記載例
-    //TODO ログイン画面、マイページ画面以外を追加する。
+    //TODO: ログイン画面、マイページ画面以外を追加する。
+}
+//TODO: 銘柄詳細のURLを確認
+function getTitle(pathname) {
+    if (pageTitles[pathname]) return pageTitles[pathname];
+    if (pathname,startsWith('/stocks/')) return '株価詳細';
+    return '画面名';
 }
 function Header() {
     const location = useLocation();
-    const title = pageTitles[location.pathname] || '画面名';
+    const navigate = useNavigate();
+    const title = getTitle(location.pathname);
     const userName = localStorage.getItem('userName') || 'ゲスト';
 
     const handleLogout = () => {
         localStorage.removeItem('userName')
-        window.location.href = '/'
+        Navigate('/');
     };
   return (
     <header className='header-top'>
         <div className='header-title'>{title}</div>
         <div className='header-link'>
-            <div>{userName}</div>
+            //TODO: マイページのリンク要確認
+            <Link to='/mypage'>{userName}</Link>
             <button 
                 className='logout-btn'
                 onClick={handleLogout}
             >
-            ログアウト
+                ログアウト
             </button>
         </div>
     </header>
