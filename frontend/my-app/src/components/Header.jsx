937b9eced28a07b60530edd7ca478552a63066f8
KeisukeYunoki <qifuyoumu@gmail.com>
2026-04-08 16:37:42 +0900

    微修正

--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -3,13 +3,13 @@ import '../styles/Header.css'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const pageTitles = {
-    '/header': 'ヘッダ', //記載例
-    //TODO: ログイン画面、マイページ画面以外を追加する。
+    '/header': 'ヘッダ', /*記載例*/
+    /*TODO: ログイン画面、マイページ画面以外を追加する。*/
 }
-//TODO: 銘柄詳細のURLを確認
+/*TODO: 銘柄詳細のURLを確認*/
 function getTitle(pathname) {
     if (pageTitles[pathname]) return pageTitles[pathname];
-    if (pathname,startsWith('/stocks/')) return '株価詳細';
+    if (pathname.startsWith('/stocks/')) return '株価詳細';
     return '画面名';
 }
 function Header() {
@@ -20,13 +20,13 @@ function Header() {
 
     const handleLogout = () => {
         localStorage.removeItem('userName')
-        Navigate('/');
+        navigate('/');
     };
   return (
     <header className='header-top'>
         <div className='header-title'>{title}</div>
         <div className='header-link'>
-            //TODO: マイページのリンク要確認
+            /*TODO: マイページのリンク要確認*/
             <Link to='/mypage'>{userName}</Link>
             <button 
                 className='logout-btn'
